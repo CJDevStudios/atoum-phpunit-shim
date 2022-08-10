bf104eee11dded7c67fd769862c465f7ec630fce
Curtis Conard <cconard96@gmail.com>
2022-08-09 20:56:46 -0400

    Fix autoload

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,5 +3,7 @@
 use CJDevStudios\AtoumShim\Atoum;
 
 // Use composer autoloader
-require_once __DIR__ . '/../vendor/autoload.php';
+if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
+    require_once __DIR__ . '/../vendor/autoload.php';
+}
 class_alias(Atoum::class, 'atoum\atoum');
